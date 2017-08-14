91bbdcdd28f84b46a343b972657036cfea7d1910
Charlie Windolf <cwindolf95@gmail.com>
2017-08-14 11:36:12 -0400

    read test better

--- util.ml
+++ util.ml
@@ -7,8 +7,8 @@ let ( @@ ) f x = f x
 let check expr name =
     match expr with
     | True -> 
-        let _ = print_endline @@ name ^ " passed." in
+        let _ = print_endline @@ "  OK   : " ^ name ^ " passed." in
         True
     | False ->
-        let _ = print_endline @@ name ^ " failed" in
+        let _ = print_endline @@ "~FAIL~ : " ^ name ^ " failed" in
         False
