3a418a92c2d374e48686d5a7f301b761ea2377ba
Charlie Windolf <cwindolf95@gmail.com>
2017-08-17 16:54:23 -0400

    approximate pi in exp time lol

--- pi.ml
+++ pi.ml
@@ -1,4 +1,3 @@
-open Bool
 open Cnt
 open Nat
 open Rat
@@ -10,16 +9,16 @@ open Rat
 
 let bbp (prec : cnt) : rat =
     let p (k : cnt) =
-        let _ = print_endline ("p " ^ Nat.(Nat.to_str (Nat k))) in
+        let eightk = Cnt.(eight * k) in
         Rat.( (Int.one, Cnt.sixteen ** k)
-            * ( (Int.four, Cnt.(eight * k + Cnt.One))
-              - (Int.two, Cnt.(eight * k + Cnt.four))
-              - (Int.one, Cnt.(eight * k + Cnt.five))
-              - (Int.one, Cnt.(eight * k + Cnt.six)))) in
+            * ( (Int.four, Cnt.(eightk + Cnt.One))
+              - (Int.two, Cnt.(eightk + Cnt.four))
+              - (Int.one, Cnt.(eightk + Cnt.five))
+              - (Int.one, Cnt.(eightk + Cnt.six)))) in
     let rec bbp_helper (k : cnt) (acc : rat) : rat =
         match Cnt.(k >= prec) with
-        | True -> acc
-        | False -> bbp_helper (Cnt.S k) (least_terms ((p k) + acc)) in
+        | true -> acc
+        | false -> bbp_helper (Cnt.S k) (least_terms ((p k) + acc)) in
     (Rat.of_int Int.three) + (bbp_helper One (Rat.of_int Int.IZero))
 
 
@@ -27,12 +26,15 @@ let wallis (iters : nat) : rat =
     let double = Nat.(two * iters) in
     let rec tail k acc =
         match k < double with
-        | False -> acc
-        | True -> let ik = Int.of_nat k in
-                  tail (Nat.inc (Nat.inc k)) Int.((ik * ik), (as_cnt ((ik - one) * (ik + one)))) in
+        | false -> acc
+        | true -> let ik = Int.of_nat k in
+                  tail (Nat.inc (Nat.inc k)) (acc * Int.((ik * ik), (as_cnt ((ik - one) * (ik + one))))) in
     tail Nat.two Rat.two
 
+
 let _ =
-    let prec = Cnt.(S One) in
-    let _ = Rat.to_dec_str ~prec:prec (wallis (Nat prec)) |> print_endline in
-    Rat.to_dec_str ~prec:prec (bbp prec) |> print_endline
+    let prec = Nat.ten in
+    let _ = print_endline "Wallis" in
+    let _ = Rat.to_dec_str ~prec:prec (wallis Nat.two) |> print_endline in
+    let _ = print_endline "BBP" in
+    Rat.to_dec_str ~prec:prec (bbp Cnt.two) |> print_endline
